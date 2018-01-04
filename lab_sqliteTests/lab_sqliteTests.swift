65bc5d10bc1a48db7d4e35951ec6455845d25cf3
r <r@rs-mac.local>
2018-01-04 09:17:13 -0800

    Initial Commit

---
+++ lab_sqliteTests/lab_sqliteTests.swift
@@ -0,0 +1,36 @@
+//
+//  lab_sqliteTests.swift
+//  lab_sqliteTests
+//
+//  Created by r on 04/01/2018.
+//  Copyright © 2018 r. All rights reserved.
+//
+
+import XCTest
+@testable import lab_sqlite
+
+class lab_sqliteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
